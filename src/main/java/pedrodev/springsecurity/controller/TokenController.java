48515a169429bc382cb6ab0441cebde97da0b920
Pedro-Reis11 <pedroverissimoreis@gmail.com>
2026-06-15 15:47:03 -0300

    feat: create users

--- src/main/java/pedrodev/springsecurity/controller/TokenController.java
+++ src/main/java/pedrodev/springsecurity/controller/TokenController.java
@@ -1,19 +1,25 @@
 package pedrodev.springsecurity.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pedrodev.springsecurity.controller.dto.LoginRequest;
 import pedrodev.springsecurity.controller.dto.LoginResponse;
+import pedrodev.springsecurity.entity.Role;
+import pedrodev.springsecurity.entity.User;
 import pedrodev.springsecurity.repository.UserRepository;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
@@ -37,11 +43,18 @@ public class TokenController {
 
         var now = Instant.now();
         var expiresIn = 300L;
+
+        var scope = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(Collectors.joining(""));
+
         var claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .subject(user.get().getId().toString())
                 .expiresAt(now.plusSeconds(expiresIn))
                 .issuedAt(now)
+                .claim("scope", scope)
                 .build();
 
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
@@ -49,5 +62,11 @@ public class TokenController {
         return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
     }
 
+    @GetMapping("/users")
+    @PreAuthorize("hasAuthority('SCOPE.ADMIN')")
+    public ResponseEntity<List<User>> listUsers(){
+        var users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
 
 }
